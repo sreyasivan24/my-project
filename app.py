d8f5bfa82c626a995011a75683628f487e4a7a69
sreyasivan24 <sreyasivan24@gmail.com>
2026-04-15 15:27:21 +0530

    First Flask web page project

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! This is my first web page using Python Flask."
+
+if __name__ == "__main__":
+    app.run(debug=True)
