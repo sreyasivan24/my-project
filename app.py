acad1024aeacb639b549e0bc4a47599c66acfe37
sreyasivan24 <sreyasivan24@gmail.com>
2026-04-16 16:44:41 +0530

    simple web page

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-a = 5
-b = 3
-sum = a + b
-print("Sum is:", sum)
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    weather = None
+
+    if request.method == "POST":
+        city = request.form.get("city")
+        
+        # Simple dummy weather (for now)
+        weather = f"Weather in {city}: Sunny ☀️, 50°C"
+
+    return render_template("index.html", weather=weather)
+
+if __name__ == "__main__":
+    app.run(debug=True)
