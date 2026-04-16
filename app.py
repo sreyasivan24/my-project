a0e0581dc466159d7f7ed0793aae005042eff425
sreyasivan24 <sreyasivan24@gmail.com>
2026-04-16 14:37:56 +0530

    app

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello! This is my first web page using Python Flask."
-
-if __name__ == "__main__":
-    app.run(debug=True)
+print("Hello World")
