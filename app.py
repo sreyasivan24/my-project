633f4c99fad74c1f17d9d7d86838d19264bde988
sreyasivan24 <sreyasivan24@gmail.com>
2026-05-05 17:04:49 +0530

    changes done

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 # Dummy weather data
 weather_data = [
     {"status": "☀️ Sunny", "temp": "20°C"},
-    {"status": "🌧️ Rainy", "temp": "20°C"},
-    {"status": "⛅ Cloudy", "temp": "39°C"},
-    {"status": "🌩️ Stormy", "temp": "25°C"},
+    {"status": "🌧️ Rainy", "temp": "40°C"},
+    {"status": "⛅ Cloudy", "temp": "29°C"},
+    {"status": "🌩️ Stormy", "temp": "10°C"},
 ]
 
 @app.route("/", methods=["GET", "POST"])
@@ -43,4 +43,3 @@ if __name__ == "__main__":
     debug_mode = os.getenv("FLASK_DEBUG", "False") == "True"
     app.run(debug=debug_mode)
 
-#this is a newly done
