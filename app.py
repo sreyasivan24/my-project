6ffe128133d689e0abbb30e877b62c4f5b6256dd
sreyasivan24 <sreyasivan24@gmail.com>
2026-05-05 17:51:36 +0530

    new

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 
 # Dummy weather data
 weather_data = [
-    {"status": "☀️ Sunny", "temp": "20°C"},
-    {"status": "🌧️ Rainy", "temp": "40°C"},
+    {"status": "☀️ Sunny", "temp": "10°C"},
+    {"status": "🌧️ Rainy", "temp": "20°C"},
     {"status": "⛅ Cloudy", "temp": "29°C"},
-    {"status": "🌩️ Stormy", "temp": "2s0°C"},
-]
+    {"status": "🌩️ Stormy", "temp": "10°C"},
 
 @app.route("/", methods=["GET", "POST"])
 def home():
